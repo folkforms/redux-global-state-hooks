6937aecfcd580fbcbcbf9dcedfe8646c4a95b82f
folkforms <alan.bergin@gmail.com>
2023-09-04 21:26:23 +0100

    Add ApiStatus to exports

---
+++ src/api-status.js
@@ -0,0 +1,6 @@
+const IDLE = 'IDLE';
+const LOADING = 'LOADING';
+const SUCCEEDED = 'SUCCEEDED';
+const FAILED = 'FAILED';
+
+export { IDLE, LOADING, SUCCEEDED, FAILED };
--- src/redux-global-state-hooks.js
+++ src/redux-global-state-hooks.js
@@ -1,4 +1,5 @@
 import { createReducer, createHook } from './_local';
 import { createNetworkReducer, createNetworkHook } from './_network';
+import * as ApiStatus from './api-status';
 
-export { createReducer, createHook, createNetworkReducer, createNetworkHook };
+export { createReducer, createHook, createNetworkReducer, createNetworkHook, ApiStatus };
